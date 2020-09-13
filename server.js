eb145bbbde9b260cdc833061468cbf96749fefb8
Matan Crispel <matan.cris@gmail.com>
2020-09-13 14:41:07 +0300

    heroku try 2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+const path = require('path')
 
 const app = express()
 const http = require('http').createServer(app);
